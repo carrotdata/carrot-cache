f5fcf1a95178af9ed2de64a653cf4bfb46adb5c2
Vladimir Rodionov <vladrodionov@gmail.com>
2022-05-17 12:08:25 -0700

    added missing license info

--- src/main/java/com/carrot/cache/eviction/LRUEvictionPolicy.java
+++ src/main/java/com/carrot/cache/eviction/LRUEvictionPolicy.java
@@ -1,30 +1,39 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.carrot.cache.eviction;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class LRUEvictionPolicy implements EvictionPolicy {
-  private static final Logger LOG = LogManager.getLogger(LRUEvictionPolicy.class);
-
   
   public LRUEvictionPolicy() {
   }
   
   @Override
   public int getPromotionIndex(long cacheItemPtr, int cacheItemIndex, int totalItems) {
-    // promote always into the head
+    // promotes always into the head
     return 0;
   }
 
   @Override
   public int getEvictionCandidateIndex(long idbPtr, int totalItems) {
-    // evict last one
+    // evicts last one
     return totalItems -1;
   }
 
   @Override
   public int getInsertIndex(long idbPtr, int totalItems) {
-    // Insert always into the head
+    // Inserts always into the head
     return 0;
   }
 }
