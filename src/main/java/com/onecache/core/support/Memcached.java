4fb3c7668fc76ba6aae7c52c4aae77776aa3fc8e
Vladimir Rodionov <vladrodionov@gmail.com>
2024-03-07 11:11:15 -0800

    reverted back Memcache support

--- src/main/java/com/onecache/core/support/Memcached.java
+++ src/main/java/com/onecache/core/support/Memcached.java
@@ -1,17 +1,6 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) Onecache, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package com.onecache.core.support;
 
 import java.io.IOException;
@@ -107,15 +96,41 @@ public class Memcached {
   
   private Cache cache;
   
-  public Memcached(Cache cache) {
+  public Memcached(Cache cache) throws IOException {
     CacheConfig config = CacheConfig.getInstance();
     if(!config.isCacheTLSSupported(cache.getName())) {
-      throw new RuntimeException("thread-local storage support must be enabled");
+      throw new IOException("thread-local storage support must be enabled");
     }
     this.cache = cache;
   }
   
+  public Memcached() throws IOException {
+    Cache cache = fromConfig();
+    if (cache == null) {
+      throw new IOException("no cache was defined in the configuration file");
+    }
+    this.cache = cache;
+  }
   
+  private Cache fromConfig() throws IOException {
+    CacheConfig conf = CacheConfig.getInstance();
+   
+    String[] cacheNames = conf.getCacheNames();
+    Cache cache = null;
+    for (String name: cacheNames) {
+      if(!conf.isCacheTLSSupported(name)) {
+        conf.setCacheTLSSupported(name, true);
+      }
+      Cache c = new Cache(name);
+      if (cache != null) {
+        cache.setVictimCache(c);
+      } else {
+        cache = c;
+      }
+    }
+    return cache;
+  }
+
   /************** Storage commands ******************/
   
   /**
