f1a8914cc3af3ba95806b9c4c77de0df771911ee
Vladimir Rodionov <vladrodionov@gmail.com>
2024-06-06 15:29:20 -0700

    Change license to SSPL #364

--- src/main/java/com/carrotdata/cache/index/AbstractIndexFormat.java
+++ src/main/java/com/carrotdata/cache/index/AbstractIndexFormat.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2024-present Carrot Data, Inc. 
+ * <p>This program is free software: you can redistribute it
+ * and/or modify it under the terms of the Server Side Public License, version 1, as published by
+ * MongoDB, Inc.
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the Server Side Public License for more details. 
+ * <p>You should have received a copy of the Server Side Public License along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
--- src/test/java/com/carrotdata/cache/io/MemoryBufferInputStream.java
+++ src/test/java/com/carrotdata/cache/io/MemoryBufferInputStream.java
@@ -1,4 +1,15 @@
 /*
+ * Copyright (C) 2024-present Carrot Data, Inc. 
+ * <p>This program is free software: you can redistribute it
+ * and/or modify it under the terms of the Server Side Public License, version 1, as published by
+ * MongoDB, Inc.
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the Server Side Public License for more details. 
+ * <p>You should have received a copy of the Server Side Public License along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
